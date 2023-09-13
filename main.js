9517ec94e16279368e3f936e87325ba84eacae91
Safaafeqri89 <113602098+Safaafeqri89@users.noreply.github.com>
2023-09-13 23:54:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+/*alert ("hola todo el mundo");
+
+//variable
+let nombre = "safaa";
+
+//constantes
+const altura = 190;
+
+//mostar en consola
+console.log(nombre);
+console.log(altura);
+
+//concatenacion
+
+let concatenacion = nombre +" "+ altura;
+
+let datos =document.querySelector("#datos");
+datos.innerHTML=`
+<H1>MI NOMBRE:${concatenacion}</H1>`;
+
+*/
+
+
+/*let nombre="safaa";
+let edad=18;
+const altura=180;
+document.write(nombre,altura,edad);
+*/
+
+ /*             
+
+let numA=10;
+let numB=12;
+let resultado= numA+numB;
+ document.write(resultado);
+*/
+
+  /*usuario falso o correcto en javascript */    
+
+
+  let usuario="user";
+  let code="user"
+
+   let resultado = usuario==code;
+
+     if(resultado===true)
+{
+
+    document.write('login correcto');
+    
+    }
+
+    
+    if(resultado===false)
+    {
+    
+        document.write('login incorrecto');
+        
+        }
